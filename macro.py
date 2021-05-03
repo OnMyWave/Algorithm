d8a7c462f90b35c05350e4cafe9c5d047cb6fd06
OnMyWave <63710797+OnMyWave@users.noreply.github.com>
2021-05-04 00:08:48 +0900

    Update macro.py

--- macro.py
+++ macro.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome('C:/Users/JongGeun/Desktop/CODING/chromedriver.exe')
 
 driver.get("https://license.korcham.net/kor/member/login.jsp")
 
-driver.find_element_by_id('uid').send_keys('johnbr1212')
+driver.find_element_by_id('uid').send_keys('id')
 driver.find_element_by_id('upwd').send_keys('password')
 
 driver.find_element_by_xpath('//*[@id="idMemberLogin"]/div[1]/div[1]/input').click()
