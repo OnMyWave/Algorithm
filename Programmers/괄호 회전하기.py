ddc223af572841518c3333b82f2f84a12bfc272f
JongGeun Lee <johnbr1212@naver.com>
2021-05-08 22:51:00 +0900

    create 괄호 회전하기.py

---
+++ Programmers/괄호 회전하기.py
@@ -0,0 +1,21 @@
+def isCorrectBracket(s):
+    type_a = [] # []
+    type_b = [] # ()
+    type_c = [] # {}
+    for bracket in s:
+        if bracket in ['[',']']:
+            type_a.append(bracket)
+        elif bracket in ['(',')']:
+            type_b.append(bracket)
+        else:
+            type_c.append(bracket)
+    for bracket in s:
+        if bracket == ']':
+            
+            
+
+def solution(s):
+
+    for bracket in s:
+        if 
+    return answer
