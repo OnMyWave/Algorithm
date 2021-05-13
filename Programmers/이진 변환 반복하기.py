62edd16b799c1cdd4358dd756b3545086dab828f
JongGeun Lee <johnbr1212@naver.com>
2021-05-13 15:19:38 +0900

    Update 이진 변환 반복하기.py

--- Programmers/이진 변환 반복하기.py
+++ Programmers/이진 변환 반복하기.py
@@ -1,19 +1,30 @@
-def solution(s):
+
+def BinaryToDecimal(binary): 
+    decimal = 0 
+    for digit in binary: 
+        decimal = decimal*2 + int(digit) 
+    return str(decimal)
+
+def solution(binary):
     zero_cnt = 0
     num = 0
+    s = binary
     while s != '1' :
-        new_s = ''
-        for b in s:
-            if b == '1':
-                new_s += '1'
-            else:
-                zero_cnt +=1
+        if '0' not in s:
+            s = str(len(s))
+        else:
+            new_s = ''
+            for b in s:
+                if b == '1':
+                    new_s += b
+                elif b == '0':
+                    zero_cnt +=1
+                s = str(len(new_s))
+        s = bin(int(s))[2:]
+
         num += 1
-        s = bin(int(new_s))
-        print(num)
-        print(s)
-        print()
+
+        
     answer = [num,zero_cnt]
     return answer
 
-print(solution("110010101001"))
