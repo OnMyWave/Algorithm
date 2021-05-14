02dd47bad6abee1d1af2f8dc47315152332df070
JongGeun Lee <johnbr1212@naver.com>
2021-05-14 14:23:38 +0900

    Update 방문 길이.py

--- Programmers/방문 길이.py
+++ Programmers/방문 길이.py
@@ -1,13 +1,11 @@
 def solution(dirs):
     answer = 0
-    current_xy =[0,0]
+    current_xy = [0,0]
     visited_edge = []
     cnt = 0 
     
     for dir in dirs : # direction
-        x = current_xy[0]
-        y = current_xy[1]
-        temp = [x,y]
+        temp = current_xy[:] # deep copy
         if dir == 'U':
             current_xy[1] += 1
         elif dir == 'D':
@@ -16,14 +14,29 @@ def solution(dirs):
             current_xy[0] += 1
         else:
             current_xy[0] -= 1
+        if current_xy[0] < 0 :
+            if current_xy[1] < 0 :
+                current_xy[0] = max(-5,current_xy[0])
+                current_xy[1] = max(-5,current_xy[1])
+            else:
+                current_xy[0] = max(-5,current_xy[0])
+                current_xy[1] = min(5,current_xy[1])
+        else:
+            if current_xy[1] < 0 :
+                current_xy[0] = min(5,current_xy[0])
+                current_xy[1] = max(-5,current_xy[1])
+            else:
+                current_xy[0] = min(5,current_xy[0])
+                current_xy[1] = min(5,current_xy[1])
         
-        edge = sorted([current_xy,temp], key = lambda x: (x[0],x[1]))
+        if dir == 'D' or dir == 'L':
+            edge = [current_xy[:],temp[:]]
+        else:
+            edge = [temp[:], current_xy[:]]
+        
         if edge not in visited_edge:
-            cnt += 1 
-        print(visited_edge)
-        print()
-        visited_edge.append(edge.copy())
+            if edge[0] != edge[1]:
+                visited_edge.append(edge.copy())
+                cnt +=1
         
-
-            
     return cnt
