a84d9df07788afcf55e5a474a2ee3bf99d222fb1
JongGeun Lee <johnbr1212@naver.com>
2021-06-04 10:20:21 +0900

    2021 06 04

--- Programmers/124 나라의 숫자.py
+++ Programmers/124 나라의 숫자.py
@@ -1,35 +1,13 @@
+def convert(n):
+    T = "124"
+    div, mod = divmod(n-1,3)
+    if div == 0:
+        return T[mod]
+    else:
+        return convert(div) + T[mod]
+
 def solution(n):
-    answer = ''
-    divisor = 1
-    cnt = 0
-
-    while n // divisor :
-        divisor *= 3
-        cnt += 1
+    answer = convert(n)
+  
         
-    three_list = [3**i for i in range(1,20)] # 11억 6천
-    if n in three_list :
-        cnt -= 1
-    
-    for i in range(cnt-1,-1,-1):
-    # for i in range(1,cnt+1):
-        a = n // (3**i)
-        if a == 1:
-            answer += "1"
-        elif a == 2:
-            answer += "2"
-        else :
-            answer += "4"
-        n = n - a*(3**i)
-        
-    return answer[::-1]
-    
-def sigma(n):
-    sum = 0
-    i = 1
-    while sum < n:
-        sum = sum + 3**i
-        i += 1
-    return i-1
-
-print(sigma(13))
+    return answer
