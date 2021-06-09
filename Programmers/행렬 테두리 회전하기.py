b0e5eb566890a36a10e5329f968a36d459fbdb25
JongGeun Lee <johnbr1212@naver.com>
2021-06-09 15:44:18 +0900

    2021 06 09
    chang

--- Programmers/행렬 테두리 회전하기.py
+++ Programmers/행렬 테두리 회전하기.py
@@ -1,34 +1,46 @@
+import sys
+
 def solution(rows, columns, queries):
     answer = []
     
     # matrix 정의
     matrix = [[columns * i + j for j in range(1,columns+1)] for i in range(rows)] 
-    new_matrix = matrix.copy()
+    copyed_matrix = [[0 for _ in range(columns)] for _ in range(rows)]
     
     # matrix rotation 시키기
     for rotation in queries:
-        changed_number = []
-        x1, x2 = rotation[0]-1, rotation[2]-1
-        y1, y2 = rotation[1]-1, rotation[3]-1
+        minimum = sys.maxsize
+
+        x1 = rotation[0]-1
+        x2 = rotation[2]-1
+        y1 = rotation[1]-1
+        y2 = rotation[3]-1
         dx = x2-x1
         dy = y2-y1
-        
+        # copyed_matrix 값 복사
+        for i in range(dy):
+            copyed_matrix[x1][y1+i] = matrix[x1][y1+i]
+            copyed_matrix[x2][y2-i] = matrix[x2][y2-i]
+            
+
+        # column 방향
+        for j in range(dx):
+            copyed_matrix[x2-j][y1] = matrix[x2-j][y1]
+            copyed_matrix[x1+j][y2] = matrix[x1+j][y2]
+    
+
         # row 방향
         for i in range(1,dy+1):
-            new_matrix[x1][y1+i] = matrix[x1][y1+i-1]
-            new_matrix[x2][y2-i] = matrix[x2][y2-i+1]
-            changed_number.append(new_matrix[x1][y1+i])
-            changed_number.append(new_matrix[x2][y2-i])
+            matrix[x1][y1+i] = copyed_matrix[x1][y1+i-1]
+            matrix[x2][y2-i] = copyed_matrix[x2][y2-i+1]
+            minimum = min(matrix[x1][y1+i],matrix[x2][y2-i],minimum)
 
         # column 방향
         for j in range(1,dx+1):
-            new_matrix[x2-j][y1] = matrix[x2-j+1][y1]
-            new_matrix[x1+j][y2] = matrix[x1+j-1][y2]
-            changed_number.append(new_matrix[x2-j][y1])
-            changed_number.append(new_matrix[x1+j][y2])
+            matrix[x2-j][y1] = copyed_matrix[x2-j+1][y1]
+            matrix[x1+j][y2] = copyed_matrix[x1+j-1][y2]
+            minimum = min(matrix[x2-j][y1],matrix[x1+j][y2],minimum)
             
-        print(changed_number)
-        answer.append(min(changed_number))
-        matrix = new_matrix.copy()
+        answer.append(minimum)
         
     return answer
