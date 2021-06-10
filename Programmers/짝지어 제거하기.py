51fe413484ddaf1ad761c9b1121be1aaa5031208
JongGeun Lee <johnbr1212@naver.com>
2021-06-10 13:21:16 +0900

    2021 06 10

--- Programmers/짝지어 제거하기.py
+++ Programmers/짝지어 제거하기.py
@@ -1,7 +1,37 @@
+# 무지성 침팬지 
 def solution(s):
+    cnt = True
+    
+    while cnt :
+        cnt = 0
+        try:
+            for i in range(len(s)-1):
+                if s[i] == s[i+1]:
+                    s = s[:i] + s[i+2:]
+                    cnt += 2
+        except:
+            pass
 
-    while True:
-        new_s = ""
-        for i in range(len(s)):
-            
+    if s == "":
+        return 1
+    else:
+        return 0 
 
+print(solution("abbaaaccddccee"))
+
+# stack 구조 이용
+def solution(s):
+    stack = []
+    
+    for c in s:
+        if not stack :
+            stack.append(c)
+        elif c == stack[-1] :
+            stack.pop()
+        else :
+            stack.append(c)
+    
+    if not stack :
+        return 1
+
+    return 0
