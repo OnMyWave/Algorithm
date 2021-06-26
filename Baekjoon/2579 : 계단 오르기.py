70e40687f02c2d8ffd48e40c4b2f7969ccc74f65
JongGeun Lee <johnbr1212@naver.com>
2021-06-26 15:55:00 +0900

    2021 06 26
    DP

--- Baekjoon/2579 : 계단 오르기.py
+++ Baekjoon/2579 : 계단 오르기.py
@@ -3,3 +3,12 @@ stairs = []
 
 for _ in range(N):
     stairs.append(int(input()))
+
+dp = [[0,0],[stairs[0],stairs[0]]]
+
+for i in range(2,len(stairs)+1):
+    dp.append([max(dp[i-2])+stairs[i-1],stairs[i-1]+dp[i-1][0]])
+
+print(max(dp[-1]))
+
+
