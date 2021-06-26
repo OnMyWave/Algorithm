e0fa467ccc77b1c1bd1658dd5bdfa87f94b869b2
JongGeun Lee <johnbr1212@naver.com>
2021-06-26 16:03:24 +0900

    2021 06 26

--- Programmers/가장 큰 정사각형 찾기.py
+++ Programmers/가장 큰 정사각형 찾기.py
@@ -1,32 +1,12 @@
 def solution(board):
-    cnt = 1
-    length = len(board)
-    length1 = len(board[0])
-    max_area = 0
+    # dp - board 탐색
+    for i in range(len(board)-1):
+        for j in range(len(board[0])-1):
+            if board[i][j] * board[i+1][j] * board[i][j+1] * board[i+1][j+1] :
+                board[i+1][j+1] += min(board[i][j], board[i+1][j], board[i][j+1])
     
-    while cnt <= min(length,length1):
-        print(cnt)
-        print()
-        check = False
-        for i in range(length+1-cnt):
-            for j in range(length1+1-cnt):
-                area = 0 
-                for k in range(cnt):
-                    for l in range(cnt):
-                        area += board[i+k][j+l]
-                
-                if area == cnt**2:
-                    max_area = area
-                    check = True
-                    break
-                    
-            if check :
-                break
-                    
-                    
-        if max_area != cnt**2:
-            break
-            
-        cnt += 1
-        
-    return max_area
+    # 최댓값 찾기
+    find_max = 0
+    for i in board:
+        find_max = max(max(i),find_max)
+    return find_max**2
