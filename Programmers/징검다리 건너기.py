6ae3f83c0ebec9902bc46bf77cca8cae92afe0a4
JongGeun Lee <johnbr1212@naver.com>
2021-08-01 21:03:18 +0900

    2021 08 01

--- Programmers/징검다리 건너기.py
+++ Programmers/징검다리 건너기.py
@@ -3,24 +3,24 @@ def solution(stones, k):
     exist = True
     length = len(stones)
     while True:
-        if exist == False :
-            break
         i = -1
-        while i < length:
+        while i+k < length:
             a = 0
             for j in range(k,0,-1):
-                try:
-                    if stones[i+j]:
-                        stones[i+j] -= 1
-                        i += j
-                        break
-                    else:
-                        a += 1
-                except: 
+                if stones[i+j]:
+                    stones[i+j] -= 1
+                    i += j
+                    a = 0
+                    break
+                else:
                     a += 1
-        
             if a == k:
                 exist = False
-                
+
+        if exist == False :
+            break
+        
         answer += 1
     return answer
+
+print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1],3))
